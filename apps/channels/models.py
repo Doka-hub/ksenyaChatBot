316856fd3535f8e52d997add0483e031acfd0366
Doka <dokadair@gmail.com>
2024-10-03 21:02:45 +0600

    Change fields to text field

--- apps/channels/models.py
+++ apps/channels/models.py
@@ -5,6 +5,7 @@ from apps.utils.models import BaseModel
 
 class Channel(BaseModel):
     name = peewee.CharField()
+    telegram_chat_id = peewee.CharField()
     url = peewee.CharField(max_length=255)
     eur_amount = peewee.DecimalField(max_digits=10, decimal_places=2)
     rub_amount = peewee.DecimalField(max_digits=10, decimal_places=2)
