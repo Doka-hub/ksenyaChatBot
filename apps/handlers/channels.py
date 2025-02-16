cc5048f15012c463db6d20a1a560723bd74880b1
Doka <dokadair@gmail.com>
2025-02-16 18:01:39 +0600

    Добавлен обработчик для отправки сообщения после подписки на канал, добавлена функция получения сообщения после подписки, обработаны исключения при отправке сообщения

--- apps/handlers/channels.py
+++ apps/handlers/channels.py
@@ -1,21 +1,28 @@
 from typing import Any
 
+from aiogram.exceptions import TelegramForbiddenError
+
 from apps.channels.crud import ChannelCRUD
 from apps.channels.keyboards.inline import get_channel_link_inline_keyboard
 from apps.payments.keyboards.inline import get_payment_choose_inline_keyboard
 from apps.users.utils import have_user_active_subscription
 from apps.utils.handlers import ChatMemberHandler, MessageHandler
+from apps.utils.messages import get_after_subscribe_message
 
 
 class ChannelJoinHandler(ChatMemberHandler):
     async def handle(self):
         invite_link = await self.event.bot.export_chat_invite_link(self.event.chat.id)
+        message = await get_after_subscribe_message()
         if invite_link:
-            await self.event.bot.send_message(
-                self.from_user.id,
-                'А перейти в сам канал можно кливнув по кнопке “Богемно-нарядно”. Кликайте и переходите в канал. Вьени куа: ',
-                reply_markup=get_channel_link_inline_keyboard(invite_link),
-            )
+            try:
+                await self.event.bot.send_message(
+                    self.from_user.id,
+                    message,
+                    reply_markup=get_channel_link_inline_keyboard(invite_link),
+                )
+            except TelegramForbiddenError:
+                pass
 
 
 class ChannelRequestHandler(MessageHandler):
