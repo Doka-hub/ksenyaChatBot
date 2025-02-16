d85e5eb021f5b0f68f167d225977341ef5d76c0c
Doka <dokadair@gmail.com>
2025-02-16 23:42:01 +0600

    [PS]

--- main/loader.py
+++ main/loader.py
@@ -6,6 +6,6 @@ from aiogram.enums import ParseMode
 from .settings import Settings
 
 settings = Settings()
-bot = Bot(settings.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.MARKDOWN_V2))
+bot = Bot(settings.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.MARKDOWN))
 storage = RedisStorage.from_url(f'{settings.REDIS_HOST}:{settings.REDIS_PORT}')
 dp = Dispatcher(storage=storage)
