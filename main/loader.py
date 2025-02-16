07853fe6520696bfcdaa0c03de7601fc37106df9
Doka <dokadair@gmail.com>
2025-02-16 23:40:08 +0600

    Обновлено использование режима разметки на 'MARKDOWN_V2' в инициализации бота[PS]

--- main/loader.py
+++ main/loader.py
@@ -1,10 +1,11 @@
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.redis import RedisStorage
+from aiogram.enums import ParseMode
 
 from .settings import Settings
 
 settings = Settings()
-bot = Bot(settings.BOT_TOKEN, default=DefaultBotProperties(parse_mode='Markdown'))
+bot = Bot(settings.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.MARKDOWN_V2))
 storage = RedisStorage.from_url(f'{settings.REDIS_HOST}:{settings.REDIS_PORT}')
 dp = Dispatcher(storage=storage)
